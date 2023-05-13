4bc5b699ae3d163f383680bee2294ece2b40a270
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-13 16:16:12 +0300

    feat: add mantine

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import Main from "./features/Main/Main";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Favorites from "./features/Favorites/Favorites";
 import Vacancy from "./features/Vacancy/Vacancy";
+import {MantineProvider} from "@mantine/core";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Header/>
-            <Routes>
-                <Route path={'/'} element={<Main/>}/>
-                <Route path={'/vacancy/:id'} element={<Vacancy/>}/>
-                <Route path={'/vacancies'} element={<Main/>}/>
-                <Route path={'/favorites'} element={<Favorites/>}/>
-            </Routes>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                <Header/>
+                <Routes>
+                    <Route path={'/'} element={<Main/>}/>
+                    <Route path={'/vacancy/:id'} element={<Vacancy/>}/>
+                    <Route path={'/vacancies'} element={<Main/>}/>
+                    <Route path={'/favorites'} element={<Favorites/>}/>
+                </Routes>
+            </MantineProvider>
         </BrowserRouter>
     )
 }
