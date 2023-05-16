03696bd730eecdab83618c8d7779f87ec52ee54b
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-16 23:13:37 +0300

    feat: add pagination component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import ReactPaginate from "react-paginate";
+import "./Pagination.css";
+import {PAGINATION} from "../../utils/pagination";
 
-const Pagination = () => {
-    return (
-        <div>
-            PAGINATION 1 2  3
-        </div>
+const Pagination = ({pageCount, pageRange, onPageCallback, forcePage}: PaginationPropsType) => {
+
+    const onPageChangeHandler = (page:PaginationOnPageType) => onPageCallback(page.selected)
+    const isVisible = PAGINATION.MIN_PAGE_COUNT <= pageCount
+
+    return (<>
+            {isVisible &&
+                <ReactPaginate pageCount={pageCount}
+                               onPageChange={onPageChangeHandler}
+                               forcePage={forcePage}
+                               pageRangeDisplayed={pageRange}
+                               marginPagesDisplayed={PAGINATION.MARGIN_PAGES}
+                               breakLabel={PAGINATION.BREAK_LABEL}
+                               previousLabel={PAGINATION.PREVIOUS}
+                               nextLabel={PAGINATION.NEXT}
+                               containerClassName={'pagination'}
+                               activeLinkClassName={'active'}
+                               disabledClassName={'disabled-page'}
+                               pageLinkClassName={'item'}
+                               previousClassName={"item"}
+                               nextLinkClassName={"item"}
+                />
+            }
+        </>
     );
 };
 
 export default Pagination;
+
+type PaginationPropsType = {
+    pageCount: number
+    pageRange: number
+    onPageCallback: (p: number) => void
+    forcePage: number
+}
+export type PaginationOnPageType = {
+    selected: number
+}
