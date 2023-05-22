693156563f0c79e77c4d5722f27f473686f6beb4
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-22 21:48:18 +0300

    feat: wrap vacancies with error and plug

--- src/features/Vacancies/Vacancies.tsx
+++ src/features/Vacancies/Vacancies.tsx
@@ -1,40 +1,46 @@
 import React, {useEffect} from 'react';
-import styles from '../../App.module.css'
 import Filter from "../../components/Filter/Filter";
 import InputWithButton from "../../components/InputWithButton/InputWithButton";
 import JobList from "../JobList/JobList";
 import Pagination from "../../components/Pagination/Pagination";
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, AppStateType} from "../../redux/store";
-import {fetchVacanciesByQueryString, setVacancies} from "../../redux/jobReducer";
-import {DEFAULT_ITEM_PER_PAGE} from "../../api/api";
+import {fetchVacanciesByQueryString, JobStateStatusType} from "../../redux/jobReducer";
+import {Container} from "@mantine/core";
+import {useVacanciesStyles} from "./VacanciesStyle";
+import PageLoader from "../../components/PageLoader/PageLoader";
+import AppError from "../../components/AppError/AppError";
+import {STATUSES} from "../../const/statuses";
+import {PAGE} from "../../const/page";
 
 const Vacancies = () => {
+
+    const {classes} = useVacanciesStyles()
     const dispatch = useDispatch()
     const currentPage = useSelector<AppStateType, number>(state => state.job.page)
-    useEffect(() => {
-        dispatch<AppDispatch>(fetchVacanciesByQueryString(currentPage))
-    }, [])
+    const status = useSelector<AppStateType, JobStateStatusType>(state => state.job.status)
+    useEffect(() => dispatch<AppDispatch>(fetchVacanciesByQueryString(currentPage)), [])
     const total = useSelector<AppStateType, number>(state => state.job.total)
+    const paginationHandler = (page: number) =>  dispatch<AppDispatch>(fetchVacanciesByQueryString(page))
 
-    const totalPages = Math.ceil(total / DEFAULT_ITEM_PER_PAGE)
-    const paginationHandler = (page: number) => {
-        dispatch<AppDispatch>(fetchVacanciesByQueryString(page))
-
-    }
+    const totalPages = Math.ceil(total / PAGE.ITEM)
 
     return (
-        <div className={styles.jobSearchContainer}>
-            <Filter/>
-            <div className={styles.jobSearch}>
-                <InputWithButton/>
-                <JobList/>
-                <Pagination onPageCallback={paginationHandler}
-                            pageCount={totalPages}
-                            pageRange={3}
-                            forcePage={currentPage}/>
-            </div>
-        </div>
+        <AppError>
+            <Container className={classes.vacancyWrapper}>
+                <Filter/>
+                <PageLoader isActive={status === STATUSES.LOADING}>
+                    <Container className={classes.jobContainer}>
+                        <InputWithButton/>
+                        <JobList/>
+                        <Pagination onPageCallback={paginationHandler}
+                                    pageCount={totalPages}
+                                    pageRange={PAGE.RANGE}
+                                    forcePage={currentPage}/>
+                    </Container>
+                </PageLoader>
+            </Container>
+        </AppError>
     );
 };
 
