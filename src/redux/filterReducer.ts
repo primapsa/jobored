29ac339a432b6d0ecd4dd4c58fe0e1975648bf4f
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-22 21:41:36 +0300

    feat: add thunk to filterreducer

--- src/redux/filterReducer.ts
+++ src/redux/filterReducer.ts
@@ -1,7 +1,11 @@
 import {CataloguesType, jobAPI} from "../api/api";
-import {AppDispatch} from "./store";
+import {AppDispatch, AppStateType} from "./store";
+import {fetchVacanciesByQueryString, JobStateStatusType} from "./jobReducer";
+import {STATUSES} from "../const/statuses";
+import {addAppStatus} from "./appReducer";
+import {clearSearchRequest} from "./searchInputReducer";
 
-const initial = {
+const initial: FilterStateType = {
     fields: {
         catalogues: [],
         paymentFrom: undefined,
@@ -10,7 +14,6 @@ const initial = {
     },
     status: 'idle'
 }
-
 
 export const filterReducer = (state: FilterStateType = initial, action: ActionType): FilterStateType => {
     switch (action.type) {
@@ -25,21 +28,44 @@ export const filterReducer = (state: FilterStateType = initial, action: ActionTy
         case "ADD-PAYMENT-TO":
             return {...state, fields: {...state.fields, paymentTo: action.payload.value}}
         case "RESET-FILTER":
-            return {...state, fields: {...state.fields, currentCatalog: null, paymentFrom: undefined, paymentTo: undefined}}
+            return {
+                ...state,
+                fields: {...state.fields, currentCatalog: null, paymentFrom: undefined, paymentTo: undefined}
+            }
         default:
             return state
     }
 }
+
 const addCatalogues = (catalogues: CataloguesType[]) => ({type: 'ADD-CATALOGUES', payload: {catalogues}} as const)
-const addStatus = (status: string) => ({type: 'ADD-STATUS', payload: {status}} as const)
+const addStatus = (status: JobStateStatusType) => ({type: 'ADD-STATUS', payload: {status}} as const)
 export const addCurrentCatalog = (id: string | null) => ({type: 'ADD-CURRENT-CATALOG', payload: {id}} as const)
 export const addPaymentFromValue = (value: PaymentType) => ({type: 'ADD-PAYMENT-FROM', payload: {value}} as const)
 export const addPaymentToValue = (value: PaymentType) => ({type: 'ADD-PAYMENT-TO', payload: {value}} as const)
 export const resetFilter = () => ({type: 'RESET-FILTER'} as const)
 
-export const fetchCatalogues = () => (dispatch: AppDispatch) => {
-    jobAPI.getCatalogues().then(catalogues => dispatch(addCatalogues(catalogues)))
+export const clearSearchQueries = () => (dispatch: AppDispatch) => {
+    dispatch(resetFilter())
+    dispatch(clearSearchRequest())
+    dispatch(fetchVacanciesByQueryString())
 }
+
+export const fetchActualCatalogues = () => (dispatch: AppDispatch, getState: () => AppStateType) => {
+    const state = getState();
+    if (!state.filter.fields.catalogues.length) {
+        dispatch(fetchCatalogues())
+    }
+}
+export const fetchCatalogues = () => (dispatch: AppDispatch) => {
+    dispatch(addStatus(STATUSES.LOADING))
+    jobAPI.getCatalogues()
+        .then(catalogues => {
+            dispatch(addCatalogues(catalogues))
+            dispatch(addStatus(STATUSES.IDLE))
+        })
+        .catch(err => dispatch(addAppStatus(STATUSES.ERROR, err.message)))
+}
+
 type ActionType =
     AddCatalogType
     | AddStatusType
@@ -55,12 +81,13 @@ type AddPaymentFromValueType = ReturnType<typeof addPaymentFromValue>
 type AddPaymentToValueType = ReturnType<typeof addPaymentToValue>
 export type PaymentType = number | '' | undefined
 type ResetFilterType = ReturnType<typeof resetFilter>
-type FilterStateType = {
-    fields: {
-        catalogues: CataloguesType[]
-        paymentFrom: PaymentType
-        paymentTo: PaymentType
-        currentCatalog: string | null
-    }
-    status: string
+export type FilterFieldsType = {
+    catalogues: CataloguesType[]
+    paymentFrom: PaymentType
+    paymentTo: PaymentType
+    currentCatalog: string | null
+}
+export type FilterStateType = {
+    fields: FilterFieldsType
+    status: JobStateStatusType
 }
