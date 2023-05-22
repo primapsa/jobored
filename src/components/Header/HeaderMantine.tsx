1b460e69dd10f57601e722b4ba222e128ab331f3
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-22 21:43:31 +0300

    feat: add header mantine

--- src/components/Header/HeaderMantine.tsx
+++ src/components/Header/HeaderMantine.tsx
@@ -1,38 +1,26 @@
-import {useState} from 'react';
-import {createStyles, Header, Container, Group, Burger, rem} from '@mantine/core';
-import {useDisclosure} from '@mantine/hooks';
-import MantineLogo from '../../img/logo/joboed_logo.svg';
+import {Container, Group, Header, Title} from '@mantine/core';
+import Logo from '../../img/logo/joboredLogo.svg';
 import {NavLink} from "react-router-dom";
+import {useStyles} from "./HeaderStyle";
 
 export function HeaderSimple({links}: HeaderSimpleProps) {
-    const [opened, {toggle}] = useDisclosure(false);
-    const [active, setActive] = useState(links[0].link);
-    const {classes, cx} = useStyles();
-
+    const {classes} = useStyles();
     const items = links.map((link) => (
-        <NavLink
-            key={link.label}
-            to={link.link}
-            className={cx(classes.link, {[classes.linkActive]: active === link.link})}
-            onClick={(event) => {
-                // event.preventDefault();
-                setActive(link.link);
-            }}
-        >
+        <NavLink key={link.label} to={link.link} className = {classes.link}>
             {link.label}
         </NavLink>
     ));
 
     return (
-        <Header height={60} mb={120}>
-            <Container className={classes.header}>
-                <NavLink to={'/vacancies'}>
-                    <img src={MantineLogo}/>
+        <Header height={'100%'} className={classes.header}>
+            <Container className={classes.container}>
+                <NavLink to={'/vacancies'} className={classes.home}>
+                    <img src={Logo} alt={'logo'}/>
+                    <Title order={1} className={classes.linkHeader}>Jobored</Title>
                 </NavLink>
                 <Group spacing={5} className={classes.links}>
                     {items}
                 </Group>
-                <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm"/>
             </Container>
         </Header>
     );
@@ -42,45 +30,3 @@ interface HeaderSimpleProps {
     links: { link: string; label: string }[];
 }
 
-const useStyles = createStyles((theme) => ({
-    header: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '100%',
-    },
-
-    links: {
-        [theme.fn.smallerThan('xs')]: {
-            display: 'none',
-        },
-    },
-
-    burger: {
-        [theme.fn.largerThan('xs')]: {
-            display: 'none',
-        },
-    },
-
-    link: {
-        display: 'block',
-        lineHeight: 1,
-        padding: `${rem(8)} ${rem(12)}`,
-        borderRadius: theme.radius.sm,
-        textDecoration: 'none',
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
-        fontSize: theme.fontSizes.sm,
-        fontWeight: 500,
-
-        '&:hover': {
-            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        },
-    },
-
-    linkActive: {
-        '&, &:hover': {
-
-            color: theme.fn.variant({variant: 'light', color: theme.primaryColor}).color,
-        },
-    },
-}));
