ad99dbeab9d8b9b1c2a3099c8bb084e6259bb45c
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-22 21:37:45 +0300

    feat: implement api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
-import {log} from "util";
 
-const ACCESS_TOKEN = 'v3.r.137440105.ade1a5c7318e723eb2ec1d4a49ea83e5294212f5.0303c9a4462879e6f3cede9ac751def925c7bf24'
+
+const ACCESS_TOKEN_OLD = 'v3.r.137440105.cfa6d9a0611280f33c689bbb574efb3279a5117e.7cf2fd32207e0b95a427a5aabc4c517ffaf8e033'
+const ACCESS_TOKEN = 'v3.r.137440105.1cf0bfe81117ede77c8e782d92ab23923aaaae46.85ae533d59ce156af7473a7d6e46456059f5622b'
+
 const SECRET_KEY = 'GEU4nvd3rej*jeh.eqp'
 const API_KEY = 'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948'
 const AUTH_LOGIN = 'sergei.stralenia@gmail.com'
 const AUTH_PWD = 'paralect123'
 const CLIENT_ID = '2356'
-const DEFAULT_PAGE_NUMBER = 0
-const DEFAULT_ITEM_PER_PAGE = 50
+export const DEFAULT_PAGE_NUMBER = 0
+export const DEFAULT_ITEM_PER_PAGE = 4
 const CLIENT_HR = 0;
 
 const axiosInstance = axios.create({
@@ -25,19 +27,22 @@ export const jobAPI = {
         axiosInstance.get<GetVacanciesResponseType>(`vacancies/?count=${count}&page=${page}`)
             .then(response => response.data)
             .then(response => response.objects),
-
     getAuthToken: (): Promise<string> => axiosInstance
         .get<AuthTokenResponseType>(`oauth2/password/?login=${AUTH_LOGIN}&password=${AUTH_PWD}&client_id${CLIENT_ID}=&client_secret=${API_KEY}&hr=${CLIENT_HR}`)
         .then(response => response.data.access_token),
 
-    getVacanciesById: (ids: number[]): Promise<ResponseType> => {
+    getVacanciesById: (ids: number[], page: number = DEFAULT_PAGE_NUMBER, count: number = DEFAULT_ITEM_PER_PAGE,): Promise<VacancyResponseType[]> => {
         const idsQuery = ids.join('&ids[]=')
-        return axiosInstance.get<GetVacanciesResponseType>(`vacancies/?ids=${idsQuery}`)
-            .then(response => response.data)
-    }
+        return ids.length ? axiosInstance.get<GetVacanciesResponseType>(`vacancies/?ids[]=${idsQuery}&page=${page}&count=${count}`)
+            .then(response => response.data.objects) : Promise.resolve([])
+    },
+    getVacanciesByQueryString: (query: string):  Promise<ResponseType> =>
+        axiosInstance.get<GetVacanciesResponseType>(`vacancies/${query}`)
+        .then(response => response.data),
 
-
+    getCatalogues: (): Promise<CataloguesType[]> => axiosInstance.get('catalogues/').then(response => response.data),
 }
+
 type ResponseType = {
     objects: VacancyResponseType[]
     total: number
@@ -148,4 +153,19 @@ export type VacancyResponseType = {
     latitude: null | string,
     longitude: null | string
 
+}
+type CatalogPositionsType =  {
+    title_rus: string
+    url_rus: string
+    title:string
+    id_parent:number
+    key: number
+}
+export type CataloguesType =  {
+    title_rus: string
+    url_rus:string
+    title: string
+    title_trimmed: string
+    key: number
+    positions:CatalogPositionsType[]
 }
