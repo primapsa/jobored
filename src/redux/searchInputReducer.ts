4081e0dc5f4adb3e27ed4a379bf0b5c646efccb6
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-22 16:59:15 +0300

    feat: add complete searchinput reducer

--- src/redux/searchInputReducer.ts
+++ src/redux/searchInputReducer.ts
@@ -1,23 +1,27 @@
+import {JobStateStatusType} from "./jobReducer";
 
 const initial: searchInputStateType = {
-    query: 'a',
-    status: []
-
+    query: '',
+    status: 'idle'
 }
+
 export const searchInputReducer = (state: searchInputStateType = initial, action: ActionType) => {
-
-
     switch(action.type){
         case "ADD-SEARCH-QUERY":
             return {...state, query: action.payload.query}
+        case "CLEAR-SEARCH-QUERY":
+            return {...state, query: ''}
         default:
             return state
     }
 }
+
 export const addSearchRequest = (query: string) => ({type: 'ADD-SEARCH-QUERY', payload:{query}} as const)
+export const clearSearchRequest = () => ({type: 'CLEAR-SEARCH-QUERY'} as const)
 type searchInputStateType = {
     query: string
-    status: number[]
+    status: JobStateStatusType
 }
-type ActionType = addSearchRequestType
-type addSearchRequestType = ReturnType<typeof addSearchRequest>
+type ActionType = AddSearchRequestType | ClearSearchRequestType
+type AddSearchRequestType = ReturnType<typeof addSearchRequest>
+type ClearSearchRequestType = ReturnType<typeof clearSearchRequest>
