fd0de365ada49ee025ab5f05fbb0d6d132d773dd
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-22 21:47:37 +0300

    feat: add store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,27 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import {jobReducer} from "./jobReducer";
 import thunk from "redux-thunk";
+import {vacancyReducer} from "./vacancyReducer";
+import {searchInputReducer} from "./searchInputReducer";
+import {filterReducer} from "./filterReducer";
+import {favoriteReducer} from "./favoriteReducer";
+import {appReducer} from "./appReducer";
+
 
 const rootReducer = combineReducers(
     {
-        job: jobReducer
+        app: appReducer,
+        job: jobReducer,
+        vacancy: vacancyReducer,
+        searchInput: searchInputReducer,
+        filter: filterReducer,
+        favorite: favoriteReducer
     }
 )
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export type AppStateType = ReturnType<typeof rootReducer>
+export type AppDispatch = typeof store.dispatch | any
+// @ts-ignore
+window.store = store;
