cf2b5c9d9f0d38cc4cc1a5529681e85cfe9cf581
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-22 21:40:40 +0300

    feat: add error plug to favorites

--- src/features/Favorites/Favorites.tsx
+++ src/features/Favorites/Favorites.tsx
@@ -2,35 +2,70 @@ import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, AppStateType} from "../../redux/store";
 import {VacancyResponseType} from "../../api/api";
-import Plug from "../../components/Plug/Plug";
-import {fetchFavorite, setFavorite} from "../../redux/jobReducer";
+
 import Item from "../../components/Item/Item";
+import Pagination from "../../components/Pagination/Pagination";
+import {Container} from "@mantine/core";
+import {addPageFavorite, addPageStatus, fetchFavorite, updateFavorite} from "../../redux/favoriteReducer";
+import Plug from "../../components/Plug/Plug";
+import {useFavoritesStyles} from "./favoriteStyle";
+import {JobStateStatusType} from "../../redux/jobReducer";
+import PageLoader from '../../components/PageLoader/PageLoader';
+import AppError from "../../components/AppError/AppError";
+import {STATUSES} from "../../const/statuses";
+import {PAGE} from "../../const/page";
 
 const Favorites = () => {
+
     const dispatch = useDispatch();
-    const favorites = useSelector<AppStateType, VacancyResponseType[]>(state => state.job.favorites)
+    const {classes} = useFavoritesStyles()
+    const currentPage = useSelector<AppStateType, number>(state => state.favorite.page)
+    const total = useSelector<AppStateType, number>(state => state.favorite.total)
+
+
     useEffect(() => {
-        dispatch<AppDispatch>(fetchFavorite())
-    }, [])
-    const favoriteCallback = (id: number, isFavorite: boolean) => dispatch<AppDispatch>(setFavorite(id, isFavorite))
-    const list = favorites.map(v => <Item key={v.id}
-                                          id={v.id}
-                                          profession={v.profession}
-                                          payment_from={v.payment_from}
-                                          payment_to={v.payment_to}
-                                          type_of_work={v.type_of_work.title}
-                                          currency={v.currency}
-                                          town={v.town.title}
-                                          favorite={v.favorite}
-                                          favoriteCallback={favoriteCallback}
-                                          blankCallback={()=>{}}
-        />
-    )
+        dispatch(addPageStatus(STATUSES.LOADING))
+        dispatch<AppDispatch>(fetchFavorite(currentPage))
+    }, [currentPage])
+
+
+    const favorites = useSelector<AppStateType, VacancyResponseType[]>(state => state.favorite.vacancies)
+    const status = useSelector<AppStateType, JobStateStatusType>(state => state.favorite.status)
+    const pages = Math.ceil(total / PAGE.ITEM)
+    const onPaginatePageHandle = (page: number) => dispatch<AppDispatch>(addPageFavorite(page))
+    const favoriteCallBackHandler = (id: number, isFavorite: boolean) => dispatch<AppDispatch>(updateFavorite(id, isFavorite))
+    const list = favorites
+        .map(v => <Item key={v.id}
+                        id={v.id}
+                        profession={v.profession}
+                        payment_from={v.payment_from}
+                        payment_to={v.payment_to}
+                        type_of_work={v.type_of_work.title}
+                        currency={v.currency}
+                        town={v.town.title}
+                        favorite={v.favorite}
+                        favoriteCallback={favoriteCallBackHandler}
+                        blankCallback={() => {
+                        }}
+            />
+        )
+
     return (
-        <div>
-            {list.length ? list : <Plug/>}
-        </div>
+        <AppError>
+            <PageLoader isActive={status === STATUSES.LOADING}>
+                <Plug isActive={Boolean(total)}>
+                    <Container className={classes.wrapper}>
+                        {list}
+                        <div className={classes.pagination}>
+                            <Pagination pageCount={pages} pageRange={PAGE.RANGE}
+                                        onPageCallback={onPaginatePageHandle}
+                                        forcePage={currentPage}/>
+                        </div>
+                    </Container>
+                </Plug>
+            </PageLoader>
+        </AppError>
     );
-};
+}
 
 export default Favorites;
