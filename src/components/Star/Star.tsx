975c41d90cd749e871e6c5685f889b485de7bf01
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-23 19:12:10 +0300

    feat: make star hover only on empty star

--- src/components/Star/Star.tsx
+++ src/components/Star/Star.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import star from './../../img/icons/star.svg'
 import filledStar from './../../img/icons/starfilled.svg'
+import hoverStar from './../../img/icons/starHover.svg'
 
-const Star = ({isFilled}: StarPropsType) =>
+const Star = ({isFilled}: StarPropsType) =>{
+    const [hover, setHover] = useState(false)
+    const mouseEnterHandle = () => setHover(true)
+    const mouseLeaveHandle = () => setHover(false)
+    const style = {
+        width: '1.375rem',
+        height: '1.375rem',
+        backgroundImage: `url(${isFilled ? filledStar : hover ? hoverStar :star})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+    }
 
-    <div style={
-        {
-            backgroundImage: `url(${isFilled ? filledStar : star})`,
-            width: '1.375rem',
-            height: '1.375rem',
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center"
-        }
-    }></div>
-
+    return (
+        <div style={style} onMouseEnter={mouseEnterHandle} onMouseLeave={mouseLeaveHandle}></div>
+    )
+}
 
 export default Star;
 
