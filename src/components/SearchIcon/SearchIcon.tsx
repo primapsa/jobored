f6eb00ace27a39ba220afb189befb3679fc3b7bd
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-23 19:13:15 +0300

    feat: add searcIcon component

---
+++ src/components/SearchIcon/SearchIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Search from './../../img/icons/searchIcon.svg'
+
+const SearchIcon = () => <div style={styles}></div>
+
+const styles = {
+    width: `13px`,
+    height: `13px`,
+    backgroundImage: `url(${Search})`,
+    backgroundPosition: `center`,
+    backgroundRepeat: `no-repeat`
+}
+export default SearchIcon;
