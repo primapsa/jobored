5f547aa6d69767e00464dafc7ca69096381c7875
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-24 19:16:40 +0300

    feat: add styles

---
+++ src/features/JobList/joblistStyle.ts
@@ -0,0 +1,12 @@
+import {createStyles} from "@mantine/core";
+
+export const useJoblistStyles = createStyles((theme) => ({
+    wrapper:
+        {
+            padding: 0,
+            margin: 0,
+            display: "flex",
+            flexDirection: "column",
+            flexGrow: 1
+        }
+}));
